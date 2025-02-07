5d51b83074c2262ae9ea5ed31c2d822947f2cd6f
Yaroslav Kh <yarik77@list.ru>
2025-02-07 14:53:28 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Transform — рекомендуемая конфигурация для избежания проблем с массивами преобразований файлов
